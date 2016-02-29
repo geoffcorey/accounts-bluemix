31f7f7b2910b10656ad429bfbf8a9cd9c20f5f28
Keith Morgan <jmorgan@us.ibm.com>
2016-02-29 12:35:43 -0500

    updates

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
-  name: 'ibmcloud:accounts-bluemix',
+  name: 'kizimo:accounts-bluemix',
   summary: "Login service for Bluemix accounts",
   version: "1.0.0",
-  git: 'https://github.com/oneibmcloud/accounts-bluemix.git'
+  git: 'https://github.com/kizimo/accounts-bluemix.git'
 });
 
 Package.onUse(function(api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('ibmcloud:bluemix@1.0.0', ['client', 'server']);
+  api.use('kizimo:bluemix@1.0.0', ['client', 'server']);
 
   api.addFiles('bluemix_login_button.css', 'client');
   api.addFiles("bluemix.js");
