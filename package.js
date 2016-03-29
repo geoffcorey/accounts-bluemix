401b11c5d6d11118dc5bb54a5556cbf6567cc715
gwcorey <gwcorey@us.ibm.com>
2016-03-29 10:16:13 -0400

    use geoffcorey:bluemix

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.use('accounts-base@1.0.0', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth@1.0.0', ['client', 'server']);
-  api.use('kizimo:bluemix@1.0.0', ['client', 'server']);
+  api.use('geoffcorey:bluemix@1.0.0', ['client', 'server']);
 
   api.addFiles('bluemix_login_button.css', 'client');
   api.addFiles("bluemix.js");
